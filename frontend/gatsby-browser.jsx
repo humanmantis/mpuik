6cd9db1f9bff77456280ed3975dbcddb971d4702
Max Floreskul <maksfloreskyl@gmail.com>
2023-08-12 13:42:16 +0300

    Add analytics

--- frontend/gatsby-browser.jsx
+++ frontend/gatsby-browser.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import '@fontsource/open-sans/300.css';
 import '@fontsource/open-sans/400.css';
 import '@fontsource/open-sans/500.css';
@@ -11,8 +12,11 @@ import './src/styles/global.scss';
 // Wraps every page in a component
 export function wrapPageElement({ element, props }) {
   return (
-    <TopLayout theme={theme} {...props}>
-      {element}
-    </TopLayout>
+    <>
+      <TopLayout theme={theme} {...props}>
+        {element}
+      </TopLayout>
+      <Analytics />
+    </>
   );
 }
