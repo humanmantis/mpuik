928f635f8b11446c6e64118e94c30c72af8e118c
Max Floreskul <maksfloreskyl@gmail.com>
2023-08-30 17:48:40 +0300

    Update gtag config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -27,7 +27,9 @@ module.exports = {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
         trackingIds: [process.env.GATSBY_GOOGLE_ANALYTICS_ID],
-        head: true
+        pluginConfig: {
+          head: true,
+        }
       },
     },
     {
