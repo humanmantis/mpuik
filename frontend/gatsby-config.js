370421362920b9d8c505fba6421b2e0b7898c41b
Max Floreskul <maksfloreskyl@gmail.com>
2023-11-25 16:50:48 +0200

    move gtag to head

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       options: {
         trackingIds: [process.env.GATSBY_GOOGLE_ANALYTICS_ID],
         pluginConfig: {
-          head: false,
+          head: true,
         }
       },
     },
